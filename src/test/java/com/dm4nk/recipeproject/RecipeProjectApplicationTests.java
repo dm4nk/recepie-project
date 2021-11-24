8d47c4690737432895c4302856a694b2b9e9d41b
dm4nk <dim.xx2011@yandex.ru>
2021-11-24 17:16:13 +0400

    fail test to check circle ci

--- src/test/java/com/dm4nk/recipeproject/RecipeProjectApplicationTests.java
+++ src/test/java/com/dm4nk/recipeproject/RecipeProjectApplicationTests.java
@@ -3,11 +3,18 @@ package com.dm4nk.recipeproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class RecipeProjectApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void failTest(){
+        assertEquals(1, 2);
     }
 
 }
