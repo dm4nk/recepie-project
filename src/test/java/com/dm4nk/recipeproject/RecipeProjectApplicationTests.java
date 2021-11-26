225483da272ae82a972cce8328845bc3e7e2d3a5
dm4nk <dim.xx2011@yandex.ru>
2021-11-26 14:31:42 +0400

    added jquery and webjars

--- src/test/java/com/dm4nk/recipeproject/RecipeProjectApplicationTests.java
+++ src/test/java/com/dm4nk/recipeproject/RecipeProjectApplicationTests.java
@@ -3,8 +3,6 @@ package com.dm4nk.recipeproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class RecipeProjectApplicationTests {
 
